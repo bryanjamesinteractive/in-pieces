d670877e2fa87a7a57fa71b0a2bd80957f55df3f
Bryan James <bryanjamesdesign@gmail.com>
2017-03-23 15:49:52 +0100

    Grunt watch updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,45 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        files: {
-          'css/main.css' : 'scss/main.scss',
-          'css/test.css' : 'scss/test.scss'
+    sass: { // Begin Sass Plugin
+        dev: {
+            options: {
+                sourcemap: 'none'
+            },
+            files: [{
+                expand: true,
+                cwd: 'scss',
+                src: ['**/*.scss'],
+                dest: 'css',
+                ext: '.css'
+            }]
         }
+    },
+    postcss: { // Begin Post CSS Plugin
+      options: {
+          map: false,
+          processors: [
+              require('autoprefixer')({
+                  browsers: ['last 2 versions', 'ie >= 9']
+              })
+          ]
       }
     },
     watch: {
       css: {
-        files: '**/*.scss',
-        tasks: ['sass']
+          files: '**/*.scss',
+          tasks: ['sass', 'postcss'],
+          options: {
+              livereload: true,
+          },
       }
     }
+    
   });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Load ALL grunt tasks
+  require('load-grunt-tasks')(grunt);
+
   grunt.registerTask('default',['watch']);
-  grunt.registerTask('dev',['sass']);
+  grunt.registerTask('dev',['sass', 'watch']);
+  
 }
