1a92f65486148c4db168aee8fa24f159835293bc
Bryan James <bryanjamesdesign@gmail.com>
2017-03-23 14:59:59 +0100

    Grunt setup.

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/main.css' : 'scss/main.scss',
+          'css/test.css' : 'scss/test.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default',['watch']);
+  grunt.registerTask('dev',['sass']);
+}
